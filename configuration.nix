d4be0b89442da54a25100d4bdaad8556926be940
njrardin <nathanaelrardin@gmail.com>
2023-06-27 00:57:27 -0500

    adds meslo-lgs-nf nerd font

--- configuration.nix
+++ configuration.nix
@@ -122,6 +122,10 @@
      xclip 
   ];
 
+  fonts.fonts = with pkgs; [
+    meslo-lgs-nf
+  ];
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
