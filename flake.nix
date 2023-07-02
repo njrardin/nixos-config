b6f952f5b7ecf586b0e72e77e7d7fcbcb3d68653
njrardin <nathanaelrardin@gmail.com>
2023-07-02 17:02:10 -0500

    alters some comments

--- flake.nix
+++ flake.nix
@@ -1,9 +1,6 @@
 {
   description = "Nate Rardin's NixOS Configuration Flake";
 
-  # Inputs
-  # https://nixos.org/manual/nix/unstable/command-ref/new-cli/nix3-flake.html#flake-inputs
-
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager = {
@@ -16,14 +13,12 @@
     nixosConfigurations = {
       "njrardinMSI" = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
-	
 	modules = [
 	  ./configuration.nix
 	  home-manager.nixosModules.home-manager
 	  {
 	    home-manager.useGlobalPkgs = true;
 	    home-manager.useUserPackages = true;
-
 	    home-manager.users.njrardin = import ./home.nix;
 	  }
 	];
