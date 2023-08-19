7498989c5e3cbf5a0e546e0b8d6d014c124b712a
njrardin <nathanaelrardin@gmail.com>
2023-08-19 18:17:29 -0500

    enables cachix

--- configuration.nix
+++ configuration.nix
@@ -135,7 +135,12 @@
   nixpkgs.config.allowUnfree = true;
 
   # Enables flakes
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  nix.settings = {
+    experimental-features = [ "nix-command" "flakes" ];
+    # Cachix support
+    substituters = ["https://hyprland.cachix.org"];
+    trusted-public-keys = ["hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="];
+  };
 
   # Hyprland
   programs.hyprland = {
